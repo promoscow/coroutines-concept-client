8e48c34d418a2417d5aecf45019b3b6819a8bbc6
Vyacheslav Chernyshoff <slava_rossii@list.ru>
2026-06-13 23:35:53 +0300

    app configs separate docker / local set web client pending acquire max count 100k

--- src/main/kotlin/ru/chernyshoff/client/dao/client/configuration/WebClientConfiguration.kt
+++ src/main/kotlin/ru/chernyshoff/client/dao/client/configuration/WebClientConfiguration.kt
@@ -2,15 +2,25 @@ package ru.chernyshoff.client.dao.client.configuration
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.client.reactive.ReactorClientHttpConnector
 import org.springframework.web.reactive.function.client.WebClient
+import reactor.netty.http.client.HttpClient
+import reactor.netty.resources.ConnectionProvider
 
 @Configuration
 class WebClientConfiguration {
 
     @Bean
-    fun webClient(): WebClient =
-        WebClient
+    fun webClient(): WebClient {
+        val connectionProvider = ConnectionProvider.builder("serverConnectionPool")
+            .pendingAcquireMaxCount(100000)
+            .build()
+        val connector = ReactorClientHttpConnector(HttpClient.create(connectionProvider))
+        return WebClient
             .builder()
+            .clientConnector(connector)
             .defaultHeader("Accept", "application/json")
             .build()
+    }
+
 }
--- src/main/kotlin/ru/chernyshoff/client/dao/client/impl/ServerClientImpl.kt
+++ src/main/kotlin/ru/chernyshoff/client/dao/client/impl/ServerClientImpl.kt
@@ -1,19 +1,21 @@
 package ru.chernyshoff.client.dao.client.impl
 
 import kotlinx.coroutines.reactor.awaitSingle
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
 import ru.chernyshoff.client.dao.client.ServerClient
 
 @Component
 class ServerClientImpl(
-    private val client: WebClient
+    private val client: WebClient,
+    @Value($$"${app.server.host}") private val serverHost: String
 ) : ServerClient {
 
     override suspend fun trace(traceId: String): String =
         client
             .get()
-            .uri("http://server:8011/server/io/trace/{traceId}", traceId)
+            .uri("$serverHost/server/io/trace/{traceId}", traceId)
             .retrieve()
             .bodyToMono(String::class.java)
             .awaitSingle()
